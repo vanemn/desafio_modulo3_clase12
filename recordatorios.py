e342152d1fa0f5b1f3c047710c2cd0920207587d
Vane <vane.moralesn@gmail.com>
2024-08-08 18:46:56 -0400

    agregados los elementos de la lista "recordatorios"

---
+++ recordatorios.py
@@ -0,0 +1,10 @@
+#Lista de recordatorios  
+recordatorios = [  
+    ['2021-01-01', '11:00', 'Levantarse y ejercitar'],  
+    ['2021-01-02', '06:00', 'Empezar el año'],  # Agregando evento nuevo  
+    ['2021-07-15', '13:00', 'No hacer nada es feriado'],  
+    ['2021-09-18', '16:00', 'Ramadas'],  
+    ['2021-12-24', '22:00', 'Cena de Navidad'],  # Agregando evento nuevo  
+    ['2021-12-25', '00:00', 'Navidad'],  
+    ['2021-12-31', '22:00', 'Cena de Año Nuevo'],  # Agregando evento nuevo  
+]  
