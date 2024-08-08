21bcddebee69cbd6b58824ff18c56d23316def92
Benjamin <pardobenjamin.r@gmail.com>
2024-08-08 19:47:24 -0400

    comentarios de codigo

--- word_count.py
+++ word_count.py
@@ -1,7 +1,9 @@
 def word_count(archivo):
-    
+    #Metodo para evaluar errores
     try:
+        #Metodo para abrir archivos
         with open(archivo , "r") as file:
+            #Uso de metodo de listas
             texto = file.read()
             palabras = texto.split(" ")
             distinct_palabras = set(palabras)
@@ -9,14 +11,18 @@ def word_count(archivo):
             caracter_distinto = set(texto)
             num_caracter = len(caracter_distinto)
             return num_palabras, num_caracter
+    #Metodo para evaluar si el archivo fue encontrado
     except FileNotFoundError:
         return(f'El archivo no fue encontrado')
+    #Lanzar error visualmente
     except Exception as e:
         return(f'ocurrio un error: {e}')
 
-
+#Cargar ruta de archivo
 archivo = "lorem_ipsum.txt"
+#Mostrar resultado pasando variable a la funcion
 resultado = word_count(archivo)
 
-print(f'La cantidad de palabras disntitas es: {resultado[0]}')
+#Mostrar resultados separando tupla
+print(f'La cantidad de palabras distintas es: {resultado[0]}')
 print(f'La cantidad de caracteres distintos es: {resultado[1]}')
